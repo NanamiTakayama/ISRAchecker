9c66e9b939547f01b51ced2c65896480ff4aff13
NanamiTakayama <ns.potable@gmail.com>
2017-02-22 21:32:21 +0900

    initial

---
+++ app.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+import time
+import datetime
+import commands
+
+print("ISRA application monitoring started")
+
+while True:
+    sysctl = str(commands.getoutput("systemctl status isra-cu.service"))
+    date = str(datetime.datetime.today())    
+    print(date)
+    print(sysctl)
+    time.sleep(30)
